819e0940389a47b53a757baaa5ede537a0dfa120
USER1 <emoleg2008@gmail.com>
2024-01-10 11:02:36 +0500

    My Project

---
+++ app15/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    avarge_time = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    avarge_time = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    duration_time = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
